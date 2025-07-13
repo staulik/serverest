eeda2f8fd9b59771d42d8684bb6115a1695ca681
saulik <50931952+staulik@users.noreply.github.com>
2025-07-13 20:47:06 -0300

    Update browser_setup.py

--- aut_front/utils/browser_setup.py
+++ aut_front/utils/browser_setup.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import tempfile  # Para criar diretórios temporários únicos
 
 def setup_browser():
+    # Criando um diretório temporário único para dados do usuário
+    user_data_dir = tempfile.mkdtemp()
+
     options = Options()
-    options.add_argument('--disable-logging')  # Desativa alguns logs
-    options.add_argument('--log-level=3')  # Minimiza log (fatal apenas)
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Remove o "DevTools listening"
+    options.add_argument(f"user-data-dir={user_data_dir}")  # Especificando o diretório de dados
+    options.add_argument("--headless")  # Se precisar rodar em modo headless (sem interface gráfica)
+    options.add_argument("--no-sandbox")  # Para evitar erro de sandbox no GitHub Actions
+    options.add_argument("--disable-dev-shm-usage")  # Evita problemas de memória
 
-    service = ChromeService(ChromeDriverManager().install())
-
+    # Inicializando o ChromeDriver
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+
     return driver
