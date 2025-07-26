7908934fd8ab5a4fe63a2db930ae9081b5f77809
saulik <50931952+staulik@users.noreply.github.com>
2025-07-26 11:35:53 -0300

    screenshot.py
    Função para capturar as evidencias e anexar no Allure.

--- aut_front/utils/screenshot.py
+++ aut_front/utils/screenshot.py
@@ -1,18 +1,26 @@
 import os
 from datetime import datetime
+import allure
+from allure_commons.types import AttachmentType
 
 def salvar_print(driver, nome_base):
     # Cria o diretório de evidências, se não existir
     pasta = "reports/evidencias"
     os.makedirs(pasta, exist_ok=True)
 
-    # Gera nome do arquivo com timestamp pra evitar sobrescrever
+    # Gera nome do arquivo com timestamp
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     nome_arquivo = f"{nome_base}_{timestamp}.png"
     caminho = os.path.join(pasta, nome_arquivo)
 
     # Captura o print da tela
     driver.save_screenshot(caminho)
+
+    # Anexa no Allure
+    with open(caminho, "rb") as imagem:
+        allure.attach(imagem.read(), name=nome_base, attachment_type=AttachmentType.PNG)
+
     return caminho
+
 
 
